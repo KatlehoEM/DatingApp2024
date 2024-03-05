d3b0716b4a50770043f57f84f0de83a2e1beb7cd
KatlehoEM <katlehomak6@gmail.com>
2024-03-05 18:14:32 +0200

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,11 +12,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 var app = builder.Build();
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
 
 app.MapControllers();
 
