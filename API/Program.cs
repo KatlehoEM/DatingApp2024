11bf22e8be29d8dd60712b1073973532ee5d1432
KatlehoEM <katlehomak6@gmail.com>
2024-03-23 01:36:16 +0200

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
 // CSIP6833 (2024)
 // KE. Makhooane (2020102527)
 using API.Extensions;
+using API.Middleware;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,9 @@ builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
