015faf000e2ba82f8244ce816c8af1e36257e07b
KatlehoEM <katlehomak6@gmail.com>
2024-03-23 02:01:44 +0200

    Section 7 - Assign 2 added

--- client/src/app/errors/not-found/not-found.component.ts
+++ client/src/app/errors/not-found/not-found.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AccountService } from 'src/app/_services/account.service';
 
 @Component({
-  selector: 'app-server-error',
+  selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
 export class NotFoundComponent {
+
+  constructor(public accountService: AccountService){
+
+  }
 
 }
