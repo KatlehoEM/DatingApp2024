bde9f1e347dc33cd9b9bad7cedeb46e3531eb14d
KatlehoEM <katlehomak6@gmail.com>
2024-04-01 22:12:48 +0200

    End of Section 8

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,10 @@
 // CSIP6833 (2024)
 // KE. Makhooane (2020102527)
+using API.Data;
 using API.Extensions;
 using API.Middleware;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,5 +25,21 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
 
 app.Run();
